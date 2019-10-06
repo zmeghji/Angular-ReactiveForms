c4e397c014f52b7bccac364596b30e696590691f
Zeeshan Meghji <zeeshan_meghji@hotmail.com>
2019-10-05 20:30:09 -0400

    completed module 5

--- Demo-Start/src/app/customers/customer.component.ts
+++ Demo-Start/src/app/customers/customer.component.ts
@@ -1,7 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm, FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { NgForm, FormGroup, FormControl, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 
 import { Customer } from './customer';
+
+function emailMatch(c: AbstractControl) : {[key:string]:boolean}|null {
+  var email = c.get('email').value;
+  var confirmEmail = c.get('confirmEmail').value;
+  if (email == confirmEmail){
+    return null;
+  }
+  return {'emailMatch':true};
+
+}
+function ratingRange(c: AbstractControl) : {[key:string]:boolean}|null {
+  if (c.value !== null && (isNaN(c.value) || c.value < 1 || c.value>5)){
+    return {'range':true};
+  }
+  return null;
+}
+
+function createRatingRangeValidator(minRating:number, maxRating: number): ValidatorFn{
+  return (c: AbstractControl) : {[key:string]:boolean}|null => {
+    if (c.value !== null && (isNaN(c.value) || c.value < minRating || c.value>maxRating)){
+      return {'range':true};
+    }
+    return null;
+  }
+}
 
 @Component({
   selector: 'app-customer',
@@ -19,14 +44,31 @@ export class CustomerComponent implements OnInit {
     //   email: new FormControl(),
     //   sendCatalog: new FormControl(true)
     // });
+    var nestedFormGroup = this.formBuilder.group({
+      'email':['',[ Validators.required, Validators.email]],
+      'confirmEmail':['',[ Validators.required, Validators.email]]
+    }, {validator: emailMatch})
     this.customerForm = this.formBuilder.group({
-      'firstName':'',
-      'lastName': '',
-      'email':'',
-      'sendCatalog': true
+      'firstName':['',[ Validators.required, Validators.minLength(3)]],
+      'lastName': ['',[ Validators.required, Validators.maxLength(50)]],
+      'emailGroup':nestedFormGroup,
+      'notificationType':'email',
+      'phoneNumber': '',
+      'sendCatalog': true,
+      // 'rating':[1,ratingRange]
+      'rating': [1, createRatingRangeValidator(0,10)]
   });
   }
-
+  setNotification(notificationType: string){
+    var phoneControl = this.customerForm.get('phoneNumber');
+    if (notificationType =='text'){
+      phoneControl.setValidators([Validators.required, Validators.minLength(10)]);
+    }
+    else{
+      phoneControl.clearValidators();
+    }
+    phoneControl.updateValueAndValidity();
+  }
   populateTestData(){
     this.customerForm.setValue({
       'firstName':'Naruto',
